6aed3c2b19a405b4b2bcaf6c969b0e87c0a3577c
ricaim16~ <kibatugidey0@gmail.com>
2024-09-01 22:59:23 +0300

    comment and rating

--- client/src/hooks/useGetUserID.jsx
+++ client/src/hooks/useGetUserID.jsx
@@ -1,3 +1,20 @@
 export const useGetUserID = () => {
   return window.localStorage.getItem("userID");
 };
+
+
+// import { useState, useEffect } from 'react';
+
+// export const useGetUserID = () => {
+//   const [user, setUser] = useState(null);
+
+//   useEffect(() => {
+//     const userID = window.localStorage.getItem("userID");
+//     if (userID) {
+//       // Fetch user data from an API if needed
+//       setUser({ _id: userID }); // Example if you only store user ID
+//     }
+//   }, []);
+
+//   return { user };
+// };
